f35159bc58802e3d0d01aade9bcf7d936c884f5b
khaled ahmed <khaled.ahmed88850@gmail.com>
2025-01-27 07:21:04 +0200

    splash view logic done

--- lib/core/helper_functions/on_generate_route.dart
+++ lib/core/helper_functions/on_generate_route.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce_app/features/on_boarding/presentation/views/on_boarding_view.dart';
 import 'package:e_commerce_app/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 
@@ -5,7 +6,8 @@ Route<dynamic> onGenerateRoute(RouteSettings settings) {
   switch (settings.name) {
     case SplashView.routeName:
       return MaterialPageRoute(builder: (_) => const SplashView()); 
-   
+    case OnBoardingView.routeName :
+      return MaterialPageRoute(builder: (_) => const OnBoardingView());
     default:
       return MaterialPageRoute(builder: (_) => const Placeholder());
   }
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,10 +1,21 @@
 import 'package:e_commerce_app/core/utils/assets.dart';
+import 'package:e_commerce_app/features/on_boarding/presentation/views/on_boarding_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  void initState() {
+    excuteNavigation();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return  Column(
@@ -21,5 +32,11 @@ class SplashViewBody extends StatelessWidget {
         SvgPicture.asset(Assets.imagesSplashBottom , fit: BoxFit.fill,)
       ],
     );
+  }
+  
+  void excuteNavigation() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, OnBoardingView.routeName);
+    });
   }
 }
